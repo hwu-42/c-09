c840a003e4c96a986b12f334f09ef41a5e086948
hwu <realwuhuidong@gmail.com>
2025-01-13 11:45:26 +0100

    01done

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
     }
 
     try {
-        BitcoinExchange btc("bitcoin_prices.csv"); // Replace with your actual CSV file
+        BitcoinExchange btc("../../data.csv"); // Replace with your actual CSV file
         btc.processInputFile(argv[1]);
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
